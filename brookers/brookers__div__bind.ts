682088312e1b43e80da6a3e402a9554cd705e9f7
Brian Takita <brian.takita@gmail.com>
2024-02-04 05:48:19 -0500

    ./brookers: brookers__div__bind: remove dependency on motion: use native web animations:
    	ready__waitfor: remove try_count
    	+ animation__run: animations are controlled by memos
    	- brookers_hero__div__entry_animation_
    	- brookers_img__div__slide_animation_
    	- brookers_master__div__slide_animation__new
    	- brookers_hero__div__slide_animation__new
    	+ s180_d12_spring__keyframe_a1_
    	+ animation_o_T
    
    - ∋ @motionone/types
    
    update dependencies

--- brookers/brookers__div__bind.ts
+++ brookers/brookers__div__bind.ts
@@ -1,9 +1,18 @@
 /// <reference lib="dom" />
 import { type brookers__timeline_op_T } from '@btakita/domain--any--brookebrodack'
 import { browser_ctx, YT$_, type YT_Player, type YT_PlayerState_val_T } from '@btakita/domain--browser--brookebrodack'
-import { sleep, tup } from 'ctx-core/function'
-import { type circular_memo_T, memo_, nullish__none_, rmemo__unset, rmemo__wait, run, sig_ } from 'ctx-core/rmemo'
-import { spring, timeline } from 'motion'
+import {
+	calling,
+	type circular_memo_T,
+	memo_,
+	nullish__none_,
+	rmemo__unset,
+	rmemo__wait,
+	run,
+	sig_,
+	sleep,
+	tup
+} from 'ctx-core/rmemo'
 export async function brookers__div__bind(brookers__div:HTMLDivElement) {
 	await brookers__div__animate(brookers__div)
 	await brookers_timeline__li__init(brookers__div)
@@ -154,17 +163,11 @@ async function brookers__div__animate(brookers__div:HTMLDivElement) {
 	const brookers_img__div =
 		brookers__div.querySelector('.brookers_img__div') as HTMLElement
 	await ready__waitfor()
-	const brookers_hero__div__middle_x = brookers_hero__div__middle_x_()
-	const brookers_hero__div__entry_animation = brookers_hero__div__entry_animation_()
-	const brookers_img__div__slide_animation = brookers_img__div__slide_animation_()
-	brookers_master__div__slide_animation__new()
-	brookers_hero__div__slide_animation__new()
+	animation__run()
 	brookers__div.classList.remove('hidden')
 	async function ready__waitfor() {
-		let try_count = 0
-		while ((innerWidth__is_pending() || brookers_hero__div__is_pending_()) && try_count < 50) {
+		while ((innerWidth__is_pending() || brookers_hero__div__is_pending_())) {
 			await sleep(10)
-			try_count++
 		}
 		/**
 		 * Chrome seems to have a short period where the outerWidth is 0.
@@ -177,123 +180,253 @@ async function brookers__div__animate(brookers__div:HTMLDivElement) {
 			return !brookers_hero__div.getBoundingClientRect().width
 		}
 	}
-	function brookers_hero__div__middle_x_() {
-		const brookers_hero__div__width = brookers_hero__div.getBoundingClientRect().width
-		return (
-			brookers_hero__div__width
-				? window.innerWidth / 2 - brookers_hero__div__width / 2
-				: '50vw'
-		)
-	}
-	function brookers_hero__div__entry_animation_() {
-		setTimeout(
-			()=>{
-				brookers_hero__div.style.transform = 'translateX(' + brookers_hero__div__middle_x + ')'
-				brookers_hero__div.classList.remove('invisible')
-			}, 0)
-		return timeline([
-			[h1,
-				{
-					x: ['-25vw', 1, 0],
-					y: ['-25vh', 1, 0],
-					rotate: [-45, 10, 0],
-					opacity: [0, .5, 1]
-				},
-				{
-					duration: .65,
-					easing: 'ease-in',
-					rotate: {
-						duration: .8,
-						easing: spring({ stiffness: 300, damping: 10 }),
+	function animation__run() {
+		brookers_hero__div.style.left = '50%'
+		brookers_hero__div.style.transform = 'translateX(-50%)'
+		brookers_hero__div.classList.remove('opacity-0')
+		console.debug('animation__run|debug|1')
+		const h1__flyin_animation_o$ = h1__flyin_animation_o$_()
+		const h1__bounce_animation_o$ = h1__bounce_animation_o$_()
+		const h2__flyin_animation_o$ = h2__flyin_animation_o$_()
+		const h2__bounce_animation_o$ = h2__bounce_animation_o$_()
+		const brookers_img__div__slidein_animation_o$ = brookers_img__div__slidein_animation_o$_()
+		const brookers_img__div__slideout_animation_o$ = brookers_img__div__slideout_animation_o$_()
+		const brookers_master__div__slide_animation_o$ = brookers_master__div__slide_animation_o$_()
+		const brookers_hero__div__slide_animation_o$ = brookers_hero__div__slide_animation_o$_()
+		return rmemo__wait(memo_(()=>
+			h1__flyin_animation_o$()?.finish
+				&& h1__bounce_animation_o$()?.finish
+				&& h2__flyin_animation_o$()?.finish
+				&& h2__bounce_animation_o$()?.finish
+				&& brookers_img__div__slidein_animation_o$()?.finish
+				&& brookers_img__div__slideout_animation_o$()?.finish
+				&& brookers_master__div__slide_animation_o$()?.finish
+				&& brookers_hero__div__slide_animation_o$()?.finish),
+		finish=>finish,
+		20_000)
+		function h1__flyin_animation_o$_() {
+			return calling(memo_<animation_o_T>(
+				$=>{
+					h1.classList.remove('opacity-0')
+					const animation = h1.animate([
+						{ transform: `translate(-25vw, -25vh) rotate(-45deg)`, opacity: 0 },
+						{ transform: `translate(4px, 1px) rotate(10deg)`, opacity: 1 },
+						{ transform: `translate(40px, 10px) rotate(10deg)`, opacity: 1 },
+					], { duration: 400, fill: 'backwards', easing: 'ease-in' })
+					animation.addEventListener('finish', ()=>{
+						$._ = { ...$(), finish: true }
+					})
+					return { animation, play: true, finish: false }
+				}))
+		}
+		function h1__bounce_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>(
+				$=>{
+					if (!h1__flyin_animation_o$().finish) return
+					const keyframe_a1 = s180_d12_spring__keyframe_a1_(40, 10, 10)
+					const val = {
+						animation: h1.animate(keyframe_a1, {
+							duration: 800,
+							easing: 'ease-in'
+						}),
+						play: true,
+						finish: false
 					}
-				}
-			],
-			[h2,
-				{
-					x: ['25vw', -1, 0],
-					y: ['-25vh', 1, 0],
-					rotate: [45, -10, 0],
-					opacity: [0, .5, 1]
-				},
-				{
-					duration: .65,
-					easing: 'ease-in',
-					rotate: {
-						duration: .8,
-						easing: spring({ stiffness: 300, damping: 10 }),
+					val.animation.addEventListener('finish', ()=>{
+						$._ = {
+							...$()!,
+							finish: true
+						}
+					})
+					return val
+				}))
+		}
+		function h2__flyin_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>(
+				$=>{
+					if (!h1__flyin_animation_o$().finish) return
+					h2.classList.remove('opacity-0')
+					let val = {
+						animation: h2.animate([
+							{ transform: `translate(25vw, -25vh) rotate(45deg)`, opacity: 0 },
+							{ transform: `translate(-4px, 1px) rotate(-10deg)`, opacity: 1 },
+							{ transform: `translate(-40px, 10px) rotate(-10deg)`, opacity: 1 },
+						], { duration: 400, fill: 'backwards', easing: 'ease-in' }),
+						play: true,
+						finish: false
 					}
+					val.animation.addEventListener('finish', ()=>{
+						$._ = {
+							...$()!,
+							finish: true
+						}
+					})
+					val = {
+						...val,
+						play: h1__flyin_animation_o$().finish
+					}
+					if (val.play) {
+						val.animation.play()
+					}
+					return val
+				}))
+		}
+		function h2__bounce_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>(
+				$=>{
+					if (!h2__flyin_animation_o$()?.finish) return
+					const keyframe_a1 = s180_d12_spring__keyframe_a1_(-40, 10, -10)
+					const val = {
+						animation: h2.animate(keyframe_a1, {
+							duration: 800,
+							easing: 'ease-in'
+						}),
+						play: true,
+						finish: false
+					}
+					val.animation.addEventListener('finish', ()=>{
+						$._ = {
+							...$()!,
+							finish: true
+						}
+					})
+					return val
+				}))
+		}
+		function brookers_img__div__slidein_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>($=>{
+				if (!h2__bounce_animation_o$()?.finish) return
+				brookers_img__div.classList.remove('opacity-0')
+				const val = {
+					animation: brookers_img__div.animate([
+						{
+							transform: 'translateX(-100vw)',
+							opacity: 0
+						},
+						{
+							transform: 'translateX(25vw)',
+							opacity: 1
+						}
+					], { duration: 200, fill: 'forwards' }),
+					play: true,
+					finish: false
 				}
-			],
-		])
+				val.animation.addEventListener('finish', ()=>{
+					$._ = {
+						...$()!,
+						finish: true
+					}
+				})
+				return val
+			}))
+		}
+		function brookers_img__div__slideout_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>($=>{
+				if (!brookers_img__div__slidein_animation_o$()?.finish) return
+				const val = {
+					animation: brookers_img__div.animate([
+						{
+							transform: 'translateX(25vw)'
+						},
+						{
+							transform: 'translateX(-100vw)'
+						}
+					], {
+						delay: 2000,
+						duration: 400,
+						fill: 'forwards'
+					}),
+					play: true,
+					finish: false
+				}
+				val.animation.addEventListener('finish', ()=>{
+					brookers_img__div.remove()
+					$._ = {
+						...$()!,
+						finish: true
+					}
+				})
+				return val
+			}))
+		}
+		function brookers_master__div__slide_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>($=>{
+				if (!brookers_img__div__slideout_animation_o$()?.finish) return
+				brookers_master__div.classList.remove('opacity-0')
+				const val = {
+					animation: brookers_master__div.animate([
+						{
+							transform: `translateX(100vw)`
+						},
+						{
+							transform: 'translateX(0)'
+						}
+					], {
+						duration: 200,
+						fill: 'backwards'
+					}),
+					play: true,
+					finish: false
+				}
+				val.animation.addEventListener('finish', ()=>{
+					$._ = {
+						...$()!,
+						finish: true
+					}
+				})
+				return val
+			}))
+		}
+		function brookers_hero__div__slide_animation_o$_() {
+			return calling(memo_<animation_o_T|undefined>($=>{
+				if (!brookers_img__div__slideout_animation_o$()?.finish) return
+				const val = {
+					animation: brookers_hero__div.animate([
+						{
+							left: '50%',
+							transform: 'translateX(-50%)'
+						},
+						{
+							left: '32px',
+							transform: 'translateX(0)'
+						}
+					], { duration: 200, fill: 'forwards' }),
+					play: true,
+					finish: false
+				}
+				val.animation.addEventListener('finish', ()=>{
+					$._ = {
+						...$()!,
+						finish: true
+					}
+				})
+				return val
+			}))
+		}
 	}
-	function brookers_img__div__slide_animation_() {
-		setTimeout(
-			()=>brookers_img__div.classList.remove('hidden'),
-			brookers_hero__div__entry_animation.duration * 1000)
-		return timeline([
-			[
-				brookers_img__div,
-				{
-					x: ['-100vw', '-100vw']
-				},
-				{
-					duration: brookers_hero__div__entry_animation.duration
-				}
-			],
-			[
-				brookers_img__div,
-				{
-					x: ['-100vw', '25vw'],
-					opacity: [0, 1]
-				},
-				{
-					duration: .2
-				}
-			],
-			[
-				brookers_img__div,
-				{
-					x: [0, '-100vw'],
-				},
-				{
-					delay: 2,
-					duration: .4,
-					persist: false
-				}
-			]
-		])
+}
+// f(0) = 0; f'(0) = 0; f''(t) = -180(f(t) - 1) - 12f'(t)
+// https://www.wolframalpha.com/input?i=f%280%29+%3D+0%3B+f%27%280%29+%3D+0%3B+f%27%27%28t%29+%3D+-180%28f%28t%29+-+1%29+-+12f%27%28t%29
+// f(t) = -1/2 e^(-6 t) (-2 e^(6 t) + sin(12 t) + 2 cos(12 t))
+function s180_d12_spring__keyframe_a1_(X:number, Y:number, O:number) {
+	const keyframe_a1:Keyframe[] = []
+	keyframe_a1.push({
+		transform: `translate(${X}px, ${Y}px) rotate(${O}deg)`
+	})
+	for (let T = 1; T < 100; T++) {
+		const t = T / 100
+		const c = -.5 * Math.exp(-6 * t) * (-2 * Math.exp(6 * t) + Math.sin(12 * t) + 2 * Math.cos(12 * t))
+		keyframe_a1.push({
+			transform: `translate(${X - c * X}px, ${Y - c * Y}px) rotate(${O - c * O}deg)`
+		})
 	}
-	function brookers_master__div__slide_animation__new() {
-		setTimeout(()=>{
-			brookers_master__div.classList.remove('hidden')
-		}, brookers_img__div__slide_animation.duration)
-		return timeline([
-			[
-				brookers_master__div,
-				{
-					x: [window.innerWidth, 0],
-					y: 0,
-				},
-				{
-					delay: brookers_img__div__slide_animation.duration,
-					duration: .4
-				}
-			]
-		])
-	}
-	function brookers_hero__div__slide_animation__new() {
-		return timeline([
-			[
-				brookers_hero__div,
-				{
-					x: [brookers_hero__div__middle_x, 36],
-					y: 0,
-				},
-				{
-					delay: brookers_img__div__slide_animation.duration,
-					duration: .4
-				}
-			]
-		])
-	}
+	keyframe_a1.push({
+		transform: `translate(0, 0) rotate(0deg)`
+	})
+	return keyframe_a1
+}
+type animation_o_T = {
+	animation:Animation
+	play:boolean
+	finish:boolean
 }
